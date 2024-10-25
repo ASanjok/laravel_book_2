603c5b65dddd63a58b03711b74284a5582e20888
Aleksandrs Sloka <sloka220@gmail.com>
2024-10-25 18:54:39 +0300

    recomit

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -11,24 +11,21 @@ class ReviewController extends Controller
 {
     public function store(Request $request, $bookId)
     {
-        // Валидация входящих данных
         $request->validate([
-            'review_text' => 'required|string|max:255', // Update 'content' to 'review_text'
+            'review_text' => 'required|string|max:255', 
         ]);
 
-        // Создание нового отзыва
         Review::create([
-            'review_text' => $request->input('review_text'), // Update 'content' to 'review_text'
+            'review_text' => $request->input('review_text'), 
             'book_id' => $bookId,
-            'user_id' => Auth::id(), // Сохраняем ID текущего пользователя как автора отзыва
+            'user_id' => Auth::id(), 
         ]);
 
-        // Перенаправление обратно на страницу книги с сообщением об успехе
         return redirect()->route('books.show', $bookId)->with('success', 'Review added successfully!');
     }
     public function edit(Review $review)
     {
-        // Check if the authenticated user is the owner of the review
+        
         if ($review->user_id !== Auth::id()) {
             return redirect()->route('books.show', $review->book_id)->with('error', 'You are not authorized to edit this review.');
         }
@@ -38,17 +35,17 @@ class ReviewController extends Controller
 
     public function update(Request $request, Review $review)
     {
-        // Check if the authenticated user is the owner of the review
+        
         if ($review->user_id !== Auth::id()) {
             return redirect()->route('books.show', $review->book_id)->with('error', 'You are not authorized to update this review.');
         }
 
-        // Validate incoming data
+        
         $request->validate([
             'review_text' => 'required|string|max:255',
         ]);
 
-        // Update the review
+        
         $review->update([
             'review_text' => $request->input('review_text'),
         ]);
@@ -58,12 +55,12 @@ class ReviewController extends Controller
 
     public function destroy(Review $review)
     {
-        // Check if the authenticated user is the owner of the review
+        
         if ($review->user_id !== Auth::id()) {
             return redirect()->route('books.show', $review->book_id)->with('error', 'You are not authorized to delete this review.');
         }
 
-        // Delete the review
+        
         $review->delete();
 
         return redirect()->route('books.show', $review->book_id)->with('success', 'Review deleted successfully!');
